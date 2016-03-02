15bb5dfe15c432eea1ce9225e76de620205ae82c
Ben Iofel <iofelben@gmail.com>
2016-03-01 22:41:54 -0500

    add body-parser for json

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+    console.log(JSON.stringify(req.body));
     res.send('<h1>aids</h1>');
 });
 
