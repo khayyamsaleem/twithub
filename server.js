b58367b5a9ff1c7c2ca49b1495f14b703080628b
Ben Iofel <iofelben@gmail.com>
2016-03-02 00:42:23 -0500

    import twit.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const twitter = require('./twit.js');
 
 const tweetAt = 'benwafflez';
 
--- twit.js
+++ twit.js
@@ -1,26 +1,13 @@
 var Twitter = require('twitter');
 var client = new Twitter({
-	consumer_key: 'j5oWVtmbD3MnrGtRk0jMOEI4z', 
+	consumer_key: 'j5oWVtmbD3MnrGtRk0jMOEI4z',
 	consumer_secret: 'VfoxZbtrTjeSPVOkuzsmjUB8c6TmIHKsZIZr83MSo0CpN1x72F',
-	access_token_key: '704876576337477636-9wsUP6ZuWHsbpHhnSyiOQIkoxd5OalJ', 
+	access_token_key: '704876576337477636-9wsUP6ZuWHsbpHhnSyiOQIkoxd5OalJ',
 	access_token_secret: 'IZ06WkLDpmCzvLXf6tTKVYl893xPIV39PK7wUCHovf8qc'
 });
 
-var params = {screen_name: 'TwithubB'};
-client.get('statuses/user_timeline', params, function(error, tweets, response){
-	if (!error) {
-		console.log(tweets);
-	}
-});
+function tweet(status, callback) {
+	client.post('statuses/update', {status}, callback);
+}
 
-client.post('statuses/update', {status: "Ben's a dick"}, function(error, tweet, response){
-	if(error) console.log(error);
-	console.log(tweet)
-	console.log(response);
-});
-
-client.post('statuses/update', {in_reply_to_status_id: "@benwafflez Ben is a dick"}, function(error, tweet, response){
-	if(error) console.log(error);
-	console.log(tweet)
-	console.log(response);
-});
+module.exports = { tweet };
